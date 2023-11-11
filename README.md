88d3d8c221014d842b7fe5ec60f6416933593c4d
petter <petter.nissinen@gmail.com>
2023-11-11 21:23:10 +0200

    "pylint sun muuta"

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 # ohtuvarasto
 ![GHA_workflow_badge](https://github.com/Petter-git/ohtuvarasto/workflows/CI/badge.svg)[![codecov](https://codecov.io/gh/Petter-git/ohtuvarasto/graph/badge.svg?token=8Z69HHD5SE)](https://codecov.io/gh/Petter-git/ohtuvarasto)
+=======
+# ohtuvarasto
+![GHA_workflow_badge](https://github.com/Petter-git/ohtuvarasto/workflows/CI/badge.svg)
+>>>>>>> 10f80e1 ("pylint sun muuta")
---
+++ src/tests/stats_test.py
@@ -0,0 +1,13 @@
+import unittest
+from statistics_service import StatisticsService
+from player_reader import PlayerReader
+from player import Player
+
+class TestTest(unittest.TestCase):
+    def setUp(self):
+        self.stats = StatisticsService(
+        PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt")
+        )
+
+    def test_topscorers(self):
+        self.assertAlmostEqual(self.stats.top, 10)
