27338def0878b00dd7f6483bf81663e115b2c9a9
Petsku2 <petter.nissinen@gmail.com>
2023-11-11 22:14:20 +0200

    poistettu tiedosto

--- src/tests/stats_test.py
+++ src/tests/stats_test.py
@@ -1,13 +1,13 @@
-import unittest
-from statistics_service import StatisticsService
-from player_reader import PlayerReader
-from player import Player
+#import unittest
+#from statistics_service import StatisticsService
+#from player_reader import PlayerReader
+#from player import Player
 
-class TestTest(unittest.TestCase):
-    def setUp(self):
-        self.stats = StatisticsService(
-        PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt")
-        )
-
-    def test_topscorers(self):
-        self.assertAlmostEqual(self.stats.top, 10)
+#class TestTest(unittest.TestCase):
+ #   def setUp(self):
+  #      self.stats = StatisticsService(
+   #     PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt")
+    #    )
+#
+ #   def test_topscorers(self):
+  #      self.assertAlmostEqual(self.stats.top, 10)
